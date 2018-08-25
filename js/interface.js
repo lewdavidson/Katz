d7543f07b93ce16f7487afd92506c2c4aff98eb2
lew <lewdavidson47@gmail.com>
2018-08-25 13:06:21 -0700

    basic search results returning and displaying for breed only

--- js/interface.js
+++ js/interface.js
@@ -11,13 +11,14 @@ $(document).ready(function(){
       var coat = item.coat;
       var pattern = item.pattern;
 
-      $('#cat-card-list').append('<ul>' + '<li>' + 'Breed: ' + breed + '</li>' + '<li>' + 'Country: ' + country + '</li>' + '<li>' + 'Origin: ' + origin + '</li>' + '<li>' + 'Coat: ' + coat + '</li>' + '<li>' + 'Pattern: '+ pattern + '</li>' + '</ul>');
 
-      $('#breed-selection').append('<option ' + 'value=' + "'" + breed + "'" + '>' + breed + '</option>');
-      $('#country-selection').append('<option ' + 'value=' + "'" + country + "'" + '>' + country + '</option>');
-      $('#origin-selection').append('<option ' + 'value=' + "'" + origin + "'" + '>' + origin + '</option>');
-      $('#coat-selection').append('<option ' + 'value=' + "'" + coat + "'" + '>' + coat + '</option>');
-      $('#pattern-selection').append('<option ' + 'value=' + "'" + pattern + "'" + '>' + pattern + '</option>');
+      // $('#cat-card-list').append('<ul>' + '<li>' + 'Breed: ' + breed + '</li>' + '<li>' + 'Country: ' + country + '</li>' + '<li>' + 'Origin: ' + origin + '</li>' + '<li>' + 'Coat: ' + coat + '</li>' + '<li>' + 'Pattern: '+ pattern + '</li>' + '</ul>');
+
+      $('#breed').append('<option ' + 'value=' + "'" + breed + "'" + '>' + breed + '</option>');
+      $('#country').append('<option ' + 'value=' + "'" + country + "'" + '>' + country + '</option>');
+      $('#origin').append('<option ' + 'value=' + "'" + origin + "'" + '>' + origin + '</option>');
+      $('#coat').append('<option ' + 'value=' + "'" + coat + "'" + '>' + coat + '</option>');
+      $('#pattern').append('<option ' + 'value=' + "'" + pattern + "'" + '>' + pattern + '</option>');
     });
   });
 
@@ -32,22 +33,37 @@ $(document).ready(function(){
 });
 
 function filterCats(breed, country, origin, coat, pattern) {
-  var breedResult = document.getElementById(breed-selection);
-  // var countryResult = document.getElementById(country);
-  // var originResult = document.getElementById(origin);
-  // var coatResult = document.getElementById(coat);
-  // var patternResult = document.getElementById(pattern);
-  console.log(breedResult.options[breedResult.selectedIndex].value);
-  // console.log(countryResult.options[countryResult.selectedIndex].value);
-  // console.log(originResult.options[originResult.selectedIndex].value);
-  // console.log(coatResult.options[coatResult.selectedIndex].value);
-  // console.log(patternResult.options[patternResult.selectedIndex].value);
-};
-
-function nextPage() {
+  var breedResult = document.getElementById('breed');
+  var countryResult = document.getElementById('country');
+  var originResult = document.getElementById('origin');
+  var coatResult = document.getElementById('coat');
+  var patternResult = document.getElementById('pattern');
+  var breedSearch = breedResult.options[breedResult.selectedIndex].value;
+  var countrySearch = countryResult.options[countryResult.selectedIndex].value;
+  var originSearch = originResult.options[originResult.selectedIndex].value;
+  var coatSearch = coatResult.options[coatResult.selectedIndex].value;
+  var patternSearch = patternResult.options[patternResult.selectedIndex].value;
   $.ajax({
-    url: "https://catfact.ninja/breeds?"
+    url: "https://catfact.ninja/breeds?limit=98"
   }).done(function(data) {
-    $('#cat-card-list').append(JSON.stringify(data))
+    var info = data.data;
+    info.forEach(function(item){
+      var breed = item.breed;
+      var country = item.country;
+      var origin = item.origin;
+      var coat = item.coat;
+      var pattern = item.pattern;
+      if(breed === breedSearch) {
+        $('#cat-card-list').html('<ul>' + '<li>' + 'Breed: ' + item.breed + '</li>' + '<li>' + 'Country: ' + item.country + '</li>' + '<li>' + 'Origin: ' + item.origin + '</li>' + '<li>' + 'Coat: ' + item.coat + '</li>' + '<li>' + 'Pattern: '+ item.pattern + '</li>' + '</ul>');
+      } else if (countrySearch && country === countrySearch) {
+        var results = [];
+        results.push(item);
+        results.forEach(function(item){
+          $('#cat-card-list').append('<ul>' + '<li>' + 'Breed: ' + item.breed + '</li>' + '<li>' + 'Country: ' + item.country + '</li>' + '<li>' + 'Origin: ' + item.origin + '</li>' + '<li>' + 'Coat: ' + item.coat + '</li>' + '<li>' + 'Pattern: '+ item.pattern + '</li>' + '</ul>');
+        });
+      } else {
+        $('#cat-card-list').html('<h1>' + 'No Results Found' + '</h1>');
+      }
+    });
   });
-}
+};
