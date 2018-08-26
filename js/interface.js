230e3a1875920824754c6ed2f6503d720ffcf2df
lew <lewdavidson47@gmail.com>
2018-08-25 17:08:52 -0700

    seperate logic from interface

--- js/interface.js
+++ js/interface.js
@@ -14,6 +14,9 @@ $(document).ready(function(){
 
       $('#cat-card-list').append('<ul>' + '<li>' + 'Breed: ' + breed + '</li>' + '<li>' + 'Country: ' + country + '</li>' + '<li>' + 'Origin: ' + origin + '</li>' + '<li>' + 'Coat: ' + coat + '</li>' + '<li>' + 'Pattern: '+ pattern + '</li>' + '</ul>');
 
+      $( '#show-all').hide();
+      $('#results-by').html('All')
+
       $('#breed').append('<option ' + 'value=' + "'" + breed + "'" + '>' + breed + '</option>');
       $('#country').append('<option ' + 'value=' + "'" + country + "'" + '>' + country + '</option>');
       $('#origin').append('<option ' + 'value=' + "'" + origin + "'" + '>' + origin + '</option>');
@@ -24,6 +27,11 @@ $(document).ready(function(){
 
   $('.filter-tag').click(function(){
     $('.filter-bar').toggleClass('open-bar');
+    $("#breed").val('breed');
+    $("#coat").val('coat');
+    $("#country").val('country');
+    $("#origin").val('origin');
+    $("#pattern").val('pattern');
   });
 
   $('#search').click(function(){
@@ -32,85 +40,8 @@ $(document).ready(function(){
 
 });
 
-function filterCats(breed, country, origin, coat, pattern) {
-  var breedResult = document.getElementById('breed');
-  var countryResult = document.getElementById('country');
-  var originResult = document.getElementById('origin');
-  var coatResult = document.getElementById('coat');
-  var patternResult = document.getElementById('pattern');
-  if(breedResult.options[breedResult.selectedIndex].value) {
-    var breedSearch = breedResult.options[breedResult.selectedIndex].value;
-    $.ajax({
-      url: "https://catfact.ninja/breeds?limit=98"
-    }).done(function(data) {
-      var info = data.data;
-      info.forEach(function(item){
-        var breed = item.breed;
-        var country = item.country;
-        var origin = item.origin;
-        var coat = item.coat;
-        var pattern = item.pattern;
-        if(breed === breedSearch) {
-          $('#cat-card-list').html('<ul>' + '<li>' + 'Breed: ' + item.breed + '</li>' + '<li>' + 'Country: ' + item.country + '</li>' + '<li>' + 'Origin: ' + item.origin + '</li>' + '<li>' + 'Coat: ' + item.coat + '</li>' + '<li>' + 'Pattern: '+ item.pattern + '</li>' + '</ul>');
-          $("#breed").val('breed');
-        };
-      });
-    });
-  } else if(countryResult.options[countryResult.selectedIndex].value){
-    var countrySearch = countryResult.options[countryResult.selectedIndex].value;
-    $.ajax({
-      url: "https://catfact.ninja/breeds?limit=98"
-    }).done(function(data){
-      var info = data.data;
-      info.forEach(function(item){
-        var country = item.country;
-        if(country === countrySearch) {
-          $('#cat-card-list').append('<ul>' + '<li>' + 'Breed: ' + item.breed + '</li>' + '<li>' + 'Country: ' + item.country + '</li>' + '<li>' + 'Origin: ' + item.origin + '</li>' + '<li>' + 'Coat: ' + item.coat + '</li>' + '<li>' + 'Pattern: '+ item.pattern + '</li>' + '</ul>');
-          $("#country").val('country');
-        };
-      });
-    });
-  } else if(coatResult.options[coatResult.selectedIndex].value){
-    var coatSearch = coatResult.options[coatResult.selectedIndex].value;
-    $.ajax({
-      url: "https://catfact.ninja/breeds?limit=98"
-    }).done(function(data){
-      var info = data.data;
-      info.forEach(function(item){
-        var coat = item.coat;
-        if(coat === coatSearch) {
-          $('#cat-card-list').append('<ul>' + '<li>' + 'Breed: ' + item.breed + '</li>' + '<li>' + 'Country: ' + item.country + '</li>' + '<li>' + 'Origin: ' + item.origin + '</li>' + '<li>' + 'Coat: ' + item.coat + '</li>' + '<li>' + 'Pattern: '+ item.pattern + '</li>' + '</ul>');
-          $("#coat").val('coat');
-        };
-      });
-    });
-  } else if(originResult.options[originResult.selectedIndex].value){
-    var originSearch = originResult.options[originResult.selectedIndex].value;
-    $.ajax({
-      url: "https://catfact.ninja/breeds?limit=98"
-    }).done(function(data){
-      var info = data.data;
-      info.forEach(function(item){
-        var origin = item.origin;
-        if(origin === originSearch) {
-          $('#cat-card-list').append('<ul>' + '<li>' + 'Breed: ' + item.breed + '</li>' + '<li>' + 'Country: ' + item.country + '</li>' + '<li>' + 'Origin: ' + item.origin + '</li>' + '<li>' + 'Coat: ' + item.coat + '</li>' + '<li>' + 'Pattern: '+ item.pattern + '</li>' + '</ul>');
-          $("#origin").val('origin');
-        };
-      });
-    });
-  } else if (patternResult.options[patternResult.selectedIndex].value){
-    var patternSearch = patternResult.options[patternResult.selectedIndex].value;
-    $.ajax({
-      url: "https://catfact.ninja/breeds?limit=98"
-    }).done(function(data){
-      var info = data.data;
-      info.forEach(function(item){
-        var pattern = item.pattern;
-        if(pattern === patternSearch) {
-          $('#cat-card-list').append('<ul>' + '<li>' + 'Breed: ' + item.breed + '</li>' + '<li>' + 'Country: ' + item.country + '</li>' + '<li>' + 'Origin: ' + item.origin + '</li>' + '<li>' + 'Coat: ' + item.coat + '</li>' + '<li>' + 'Pattern: '+ item.pattern + '</li>' + '</ul>');
-          $("#pattern").val('pattern');
-        };
-      });
-    });
-  };
+function showAll() {
+  $( '#cat-result-display').hide();
+  $( "#cat-card-list" ).show();
+  $( '#show-all').hide();
 };
---
+++ js/logic.js
@@ -0,0 +1,85 @@
+function filterCats(breed, country, origin, coat, pattern) {
+  $( '#cat-result-display').empty();
+  $( "#cat-card-list" ).hide();
+  $( '#show-all').show();
+  var breedResult = document.getElementById('breed');
+  var countryResult = document.getElementById('country');
+  var originResult = document.getElementById('origin');
+  var coatResult = document.getElementById('coat');
+  var patternResult = document.getElementById('pattern');
+  if(breedResult.options[breedResult.selectedIndex].value) {
+    var breedSearch = breedResult.options[breedResult.selectedIndex].value;
+    $.ajax({
+      url: "https://catfact.ninja/breeds?limit=98"
+    }).done(function(data) {
+      var info = data.data;
+      info.forEach(function(item){
+        var breed = item.breed;
+        if(breed === breedSearch) {
+          $('#cat-result-display').append('<div>' + "<img id='cat-pic' src='https://api.thecatapi.com/v1/images/search?format=src&mime_types=image'>" + '</img>' + '<ul>' + '<li>' + 'Breed: ' + item.breed + '</li>' + '<li>' + 'Country: ' + item.country + '</li>' + '<li>' + 'Origin: ' + item.origin + '</li>' + '<li>' + 'Coat: ' + item.coat + '</li>' + '<li>' + 'Pattern: '+ item.pattern + '</li>' + '</ul>' + '</div>');
+          $('#results-by').html('Breed: ' + breedSearch);
+        };
+      });
+    });
+  };
+  if(countryResult.options[countryResult.selectedIndex].value){
+    var countrySearch = countryResult.options[countryResult.selectedIndex].value;
+    $.ajax({
+      url: "https://catfact.ninja/breeds?limit=98"
+    }).done(function(data){
+      var info = data.data;
+      info.forEach(function(item){
+        var country = item.country;
+        if(country === countrySearch) {
+          $('#cat-result-display').append('<div>' + "<img id='cat-pic' src='https://api.thecatapi.com/v1/images/search?format=src&mime_types=image'>" + '</img>' + '<ul>' + '<li>' + 'Breed: ' + item.breed + '</li>' + '<li>' + 'Country: ' + item.country + '</li>' + '<li>' + 'Origin: ' + item.origin + '</li>' + '<li>' + 'Coat: ' + item.coat + '</li>' + '<li>' + 'Pattern: '+ item.pattern + '</li>' + '</ul>' + '</div>');
+          $('#results-by').html('Country: ' + countrySearch);
+        };
+      });
+    });
+  };
+ if(coatResult.options[coatResult.selectedIndex].value){
+    var coatSearch = coatResult.options[coatResult.selectedIndex].value;
+    $.ajax({
+      url: "https://catfact.ninja/breeds?limit=98"
+    }).done(function(data){
+      var info = data.data;
+      info.forEach(function(item){
+        var coat = item.coat;
+        if(coat === coatSearch) {
+          $('#cat-result-display').append( '<div>' + "<img id='cat-pic' src='https://api.thecatapi.com/v1/images/search?format=src&mime_types=image'>" + '</img>' + '<ul>' + '<li>' + 'Breed: ' + item.breed + '</li>' + '<li>' + 'Country: ' + item.country + '</li>' + '<li>' + 'Origin: ' + item.origin + '</li>' + '<li>' + 'Coat: ' + item.coat + '</li>' + '<li>' + 'Pattern: '+ item.pattern + '</li>' + '</ul>' + '</div>');
+          $('#results-by').html('Coat: ' + coatSearch);
+        };
+      });
+    });
+  };
+  if(originResult.options[originResult.selectedIndex].value){
+    var originSearch = originResult.options[originResult.selectedIndex].value;
+    $.ajax({
+      url: "https://catfact.ninja/breeds?limit=98"
+    }).done(function(data){
+      var info = data.data;
+      info.forEach(function(item){
+        var origin = item.origin;
+        if(origin === originSearch) {
+          $('#cat-result-display').append('<div>' + "<img id='cat-pic' src='https://api.thecatapi.com/v1/images/search?format=src&mime_types=image'>" + '</img>' + '<ul>' + '<li>' + 'Breed: ' + item.breed + '</li>' + '<li>' + 'Country: ' + item.country + '</li>' + '<li>' + 'Origin: ' + item.origin + '</li>' + '<li>' + 'Coat: ' + item.coat + '</li>' + '<li>' + 'Pattern: '+ item.pattern + '</li>' + '</ul>' + '</div>' );
+          $('#results-by').html('Origin: ' + originSearch);
+        };
+      });
+    });
+  };
+  if (patternResult.options[patternResult.selectedIndex].value){
+    var patternSearch = patternResult.options[patternResult.selectedIndex].value;
+    $.ajax({
+      url: "https://catfact.ninja/breeds?limit=98"
+    }).done(function(data){
+      var info = data.data;
+      info.forEach(function(item){
+        var pattern = item.pattern;
+        if(pattern === patternSearch) {
+          $('#cat-result-display').append('<div>' + "<img id='cat-pic' src='https://api.thecatapi.com/v1/images/search?format=src&mime_types=image'>" + '</img>' + '<ul>' + '<li>' + 'Breed: ' + item.breed + '</li>' + '<li>' + 'Country: ' + item.country + '</li>' + '<li>' + 'Origin: ' + item.origin + '</li>' + '<li>' + 'Coat: ' + item.coat + '</li>' + '<li>' + 'Pattern: '+ item.pattern + '</li>' + '</ul>' + '</div>');
+          $('#results-by').html('Origin: ' + originSearch);
+        };
+      });
+    });
+  };
+};
